a080fedd034764740337146e0cb1259d510497cb
CNT-Captain <1468778953@qq.com>
2020-08-28 18:31:19 +0800

    List原理分析

---
+++ src/main/java/priv/captain/list/ListDemo.java
@@ -0,0 +1,32 @@
+package priv.captain.list;
+
+import static org.hamcrest.CoreMatchers.either;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName:ListDemo   
+ * @Description:对List的使用及原理分析
+ * @author:CNT-Captain 
+ * @date:2020年8月28日 下午4:59:53    
+ * @Copyright:2020 https://gitee.com/CNT-Captain Inc. All rights reserved.
+ */
+public class ListDemo {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * 1.底层基于数组的支持元素随机访问，其实现了标记接口RandomAccess，表示其支持随机访问。
+		 * 2.LinkedList基于链表，其实现了Deque队列。
+		 */
+		/**
+		 * 默认new空数组，第一放入时默认10，后续add都会检查是否超容量，若超容量执行grow(int minCapacity),
+		 * 扩容规则：int newCapacity = oldCapacity + (oldCapacity >> 1);
+		 */
+		List<String> arrayList = new ArrayList<>();
+		arrayList.add("666");
+		arrayList.add(2,"5555");
+		arrayList.forEach(e->{System.out.println(e);});
+	}
+}
